fc9b078f27343004862e90782a9038699a34b226
larse514 <andrew.larsen@concordusa.com>
2018-04-18 16:57:41 -0500

    init

---
+++ aardvark/aardvarkproxy/handler/logging.go
@@ -0,0 +1,27 @@
+package handler
+
+import (
+	"log"
+	"net/http"
+)
+
+type key int
+
+const (
+	requestIDKey key = 0
+)
+
+func Logging(logger *log.Logger) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			defer func() {
+				requestID, ok := r.Context().Value(requestIDKey).(string)
+				if !ok {
+					requestID = "unknown"
+				}
+				logger.Println(requestID, r.Method, r.URL.Path, r.RemoteAddr, r.UserAgent())
+			}()
+			next.ServeHTTP(w, r)
+		})
+	}
+}
